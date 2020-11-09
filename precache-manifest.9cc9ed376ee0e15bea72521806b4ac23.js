3fcb3fa612e146e0260737527e5da30023dadafd
Addy2012 <kumaranand287@gmail.com>
2020-11-09 12:07:51 +0530

    Updates

---
+++ precache-manifest.9cc9ed376ee0e15bea72521806b4ac23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15efb4a6d735e425ea6b742509dc2f5a",
+    "url": "/meghabirthday/index.html"
+  },
+  {
+    "revision": "0ff897a0b7d18a214ba8",
+    "url": "/meghabirthday/static/css/main.0817c86b.chunk.css"
+  },
+  {
+    "revision": "bb3b278b2a8efee6c178",
+    "url": "/meghabirthday/static/js/2.2b7e0751.chunk.js"
+  },
+  {
+    "revision": "2f51d09c5638a1f80deee2d36ef9b4d1",
+    "url": "/meghabirthday/static/js/2.2b7e0751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ff897a0b7d18a214ba8",
+    "url": "/meghabirthday/static/js/main.3cce9d98.chunk.js"
+  },
+  {
+    "revision": "2ddb5aa51e73b96c011a",
+    "url": "/meghabirthday/static/js/runtime-main.9ba8c66b.js"
+  },
+  {
+    "revision": "ec49c69d1ff1399515c6f9eb1c16f610",
+    "url": "/meghabirthday/static/media/Wallpaper.ec49c69d.jpg"
+  }
+]);

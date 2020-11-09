d4c773dfc0b55d96ed6a356ceb182449130d751c
Addy2012 <kumaranand287@gmail.com>
2020-11-09 15:03:40 +0530

    Updates

---
+++ precache-manifest.78f7de0bca5cde7ea6d29cca4902c288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808c130539294972938beebbae22f38f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bae2be2eda685e4e2832",
+    "url": "/static/css/main.fe042e69.chunk.css"
+  },
+  {
+    "revision": "a0dfde629b6f46e73f6c",
+    "url": "/static/js/2.9d4b1c58.chunk.js"
+  },
+  {
+    "revision": "2f51d09c5638a1f80deee2d36ef9b4d1",
+    "url": "/static/js/2.9d4b1c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae2be2eda685e4e2832",
+    "url": "/static/js/main.795f84d8.chunk.js"
+  },
+  {
+    "revision": "a6c0844c14ac977f76c1",
+    "url": "/static/js/runtime-main.7bf96654.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meghabirthday/precache-manifest.9cc9ed376ee0e15bea72521806b4ac23.js"
+  "/precache-manifest.78f7de0bca5cde7ea6d29cca4902c288.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meghabirthday/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
